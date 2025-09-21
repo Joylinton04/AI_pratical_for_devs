1db45f02e8373ab8541af9fc6233820241d3cae5
Joylinton04 <adujoy05@gmail.com>
2025-09-21 18:10:17 +0000

    connected frontend to backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,26 @@
 import express from "express";
 import type {Request, Response} from 'express'
+import cors from 'cors'
 
 
 
 const app = express()
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+
 
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello")
+})
+
+app.get('/api/data', (req: Request, res:Response) => {
+    res.json({
+        name: "Joylinton",
+        age: 21
+    })
 })
 
 
