3d61d68fb4b975942fb51b29e78f53618e2bcf02
Joylinton04 <adujoy05@gmail.com>
2025-09-28 21:10:57 +0000

    client folder resetup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
+import Chatbot from "./components/Chatbot";
 
 const App = () => {
-   return <div className="">App</div>;
+   return (
+      <div className="font-body max-w-7xl mx-auto p-6">
+         <h1 className="text-center text-3xl font-bold">AI CHATBOT POWERED BY OPENAI </h1>
+         <div className="flex justify-center items-center">
+            <Chatbot />
+         </div>
+      </div>
+   );
 };
 
 export default App;
