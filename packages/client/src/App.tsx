3217790d3562d5b39418affc83278d82f4ca0468
Joylinton04 <adujoy05@gmail.com>
2025-09-29 11:43:15 +0000

    chatbot layout

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Chatbot from "./components/Chatbot";
+import Chatbot from "./components/chat/Chatbot";
 
 const App = () => {
    return (
-      <div className="font-body max-w-7xl mx-auto p-6">
+      <div className="font-body max-w-7xl mx-auto h-screen py-2">
          <h1 className="text-center text-3xl font-bold">AI CHATBOT POWERED BY OPENAI </h1>
-         <div className="flex justify-center items-center">
+         <div className="flex justify-center items-center py-6 w-[45rem] mx-auto h-full">
             <Chatbot />
          </div>
       </div>
